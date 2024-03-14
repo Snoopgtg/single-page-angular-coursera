e0fc4449de643af50b01a9a88a08544452a95679
Taras <myemail@email.com>
2024-03-14 11:25:13 +0200

    add assignment1

---
+++ examples/assignments/assignment1/app.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+  $scope.lunchMenu = "";
+  $scope.showMessage = "";
+
+  $scope.validateOrder = function () {
+    var messageForDisplay = countMeals($scope.lunchMenu);
+    console.log(messageForDisplay);
+    $scope.showMessage = messageForDisplay;
+  };
+
+  function countMeals(string) {
+    if (string.split(',').length <= 3 && string) {
+      return "Enjoy!";
+    } else if (string.split(',').length > 3) {
+      return "Too much!";
+    }
+    return "Please enter data first";
+
+  };
+}
+
+})();
